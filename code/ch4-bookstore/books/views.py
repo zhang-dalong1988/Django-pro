89817227b93ad8afb19fbb100ff493bba1d85955
zhang-dalong1988 <zhangzhan1988@gmail.com>
2025-08-20 14:59:30 +0800

    ch14

--- code/ch4-bookstore/books/views.py
+++ code/ch4-bookstore/books/views.py
@@ -1,15 +1,22 @@
-from django.views.generic import ListView, DetailView  # 新增
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,  # new
+)
+from django.views.generic import ListView, DetailView
 
 from .models import Book
 
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):  # new
     model = Book
-    context_object_name = "book_list"  # 新增
+    context_object_name = "book_list"
     template_name = "books/book_list.html"
+    login_url = "account_login"  # new
 
 
-class BookDetailView(DetailView):  # 新增
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):  # new
     model = Book
-    context_object_name = "book"  # 新增
+    context_object_name = "book"
     template_name = "books/book_detail.html"
+    login_url = "account_login"
+    permission_required = "books.special_status"  # new
